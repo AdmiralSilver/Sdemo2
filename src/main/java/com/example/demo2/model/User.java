abfa6b51240d1e57926c6ba79c32cb30b83a4441
UlrikFa <595440@stud.hvl.no>
2023-01-12 14:28:56 +0100

    ekstern liste med thymeleaf

---
+++ src/main/java/com/example/demo2/model/User.java
@@ -0,0 +1,44 @@
+package com.example.demo2.model;
+
+public class User {
+
+    private int age;
+
+    private String firstName;
+
+    private String lastName;
+
+    public User(){
+
+    }
+
+    public User(int age, String firstName, String lastName){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
