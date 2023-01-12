15c7d75b6c1b847e0b835ef8ccc5133ef7f8e820
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-01-12 14:31:26 +0100

    ekstern liste med thymeleaf (#1)
    Co-authored-by: UlrikFa <595440@stud.hvl.no>

--- src/main/java/com/example/demo2/controller/democontroller.java
+++ src/main/java/com/example/demo2/controller/democontroller.java
@@ -1,14 +1,37 @@
 package com.example.demo2.controller;
 
+import com.example.demo2.model.User;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class democontroller {
 
+    private List<User> users;
+
+    @PostConstruct
+    private void loadUsers() {
+        User u1 = new User(29, "Ulrik", "Fagerberg");
+        User u2 = new User(40, "Erik", "F");
+        User u3 = new User(40, "Jørgen", "V");
+
+        users = new ArrayList<User>();
+
+        users.add(u1);
+        users.add(u2);
+        users.add(u3);
+    }
+
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model theModel) {
+
+        theModel.addAttribute("users", users);
+
         return "hello";
     }
 }
