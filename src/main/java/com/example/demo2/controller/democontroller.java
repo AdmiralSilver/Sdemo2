c04b21038e589d7a2a9db3b97cc0b015c3b50cc1
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-01-13 13:17:08 +0100

    Api stuff
    jaddada

--- src/main/java/com/example/demo2/controller/democontroller.java
+++ src/main/java/com/example/demo2/controller/democontroller.java
@@ -2,9 +2,12 @@ package com.example.demo2.controller;
 
 import com.example.demo2.model.User;
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +16,7 @@ import java.util.List;
 public class democontroller {
 
     private List<User> users;
-
+    private String estAge;
     @PostConstruct
     private void loadUsers() {
         User u1 = new User(29, "Ulrik", "Fagerberg");
@@ -26,12 +29,39 @@ public class democontroller {
         users.add(u2);
         users.add(u3);
     }
+    @Service
+    public class AgeService {
+        private final RestTemplate restTemplate;
+
+        public AgeService(RestTemplateBuilder restTemplateBuilder) {
+            this.restTemplate = restTemplateBuilder.build();
+        }
+        public String getAge(String firstName) {
+            String url = "https://api.agify.io?name=" + firstName;
+            return this.restTemplate.getForObject(url, String.class);
+        }
+    }
+    @PostConstruct
+    private void loadAge() {
+        // send API request to get age from api.agify.io
+        // i.e. for Ulrik -> https://api.agify.io?name=ulrik
+        // and store the result in the estAge variable
+        AgeService ageService = new AgeService(new RestTemplateBuilder());
+        String age = ageService.getAge("ulrik");
+        System.out.println(age);
+        // parse JSON response
+        // extract age
+        // store age in estAge
+        estAge = age.substring(7, 9);
+        System.out.println(estAge);
+
+    }
 
     @GetMapping("/hello")
     public String hello(Model theModel) {
 
         theModel.addAttribute("users", users);
-
+        theModel.addAttribute("estAge", estAge);
         return "hello";
     }
 }
